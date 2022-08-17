0f29975e997ca50eb4e51503f6092b535485837a
Tyrannanas <11016482@stud.hochschule-heidelberg.de>
2022-08-17 18:10:21 +0200

    Class Structure redone

--- src/BattleActor.h
+++ src/BattleActor.h
@@ -6,15 +6,23 @@
 #define RAYLIBSTARTER_BATTLEACTOR_H
 
 #include "raylib.h"
+#include "Actor.h"
+enum class DamageType
+{
+    Pierce,
+    Slash
+};
 
 struct attack
 {
     int damage;
     int maxTargets;
     int actionPoints;
+    DamageType damagetype;
+    bool state; //true active false passive
 };
 
-class BattleActor {
+class BattleActor : public Actor {
 public:
     virtual void GetCurrentAttack();
     virtual void Attack(attack currentAttack);
--- src/BattlePlayer.h
+++ src/BattlePlayer.h
@@ -8,12 +8,24 @@
 #include "BattleActor.h"
 
 
-class BattlePlayer : public BattleActor {
+class BattlePlayer final : public BattleActor {
 public:
     BattlePlayer();
-    attack moves[4];
+    attack moves[4] {Slash, Arrow,ArrowRain};
 
 private:
+    Texture2D Animations;
+
+    //Attributes
+    float LP = 200.f;
+    float AP = 5.f;
+
+    //Moves
+    attack Slash = {15,1,2, DamageType::Slash,true};
+    attack Arrow = {15,1,2, DamageType::Pierce, true};
+    attack ArrowRain = {15, 3, 5, DamageType::Pierce, true};
+
+
 
 };
 
