9213d89f2c0ad60da8fb823345cff4c9a5321c9b
Lee E <98901977+KupferLee@users.noreply.github.com>
2022-08-18 18:41:39 +0200

    switched back to enum

--- src/ActorLoredrop.cpp
+++ src/ActorLoredrop.cpp
@@ -15,17 +15,17 @@ ActorLoredrop::ActorLoredrop()
 void ActorLoredrop::Update()
 {
     // show loredrop on/off
-    if (IsKeyPressed(KEY_TAB) && this->current == 0)
+    if (IsKeyPressed(KEY_TAB) && this->currentLoredrop == WhichLoredrop::none)
     {
-        this->current = 1;
+        this->currentLoredrop = WhichLoredrop::base;
     }
-    else if (IsKeyPressed(KEY_TAB) && this->current == 1)
+    else if (IsKeyPressed(KEY_TAB) && this->currentLoredrop == WhichLoredrop::base)
     {
-        this->current = 2;
+        this->currentLoredrop = WhichLoredrop::first;
     }
-    else if (IsKeyPressed(KEY_TAB) && this->current == 2)
+    else if (IsKeyPressed(KEY_TAB) && this->currentLoredrop == WhichLoredrop::first)
     {
-        this->current = 0;
+        this->currentLoredrop = WhichLoredrop::none;
     }
 
     InternUpdate();
@@ -39,25 +39,25 @@ void ActorLoredrop::Render()
 
 void ActorLoredrop::InternUpdate()
 {
-    switch (this->current)
+    switch ((int)this->currentLoredrop)
     {
-        case 1:
+        case (int)WhichLoredrop::base:
             this->position = {(float)Katara.getPosition().x, (float)Katara.getPosition().y - 20, (float)this->loredropBase.width, (float)this->loredropBase.height};
             UpdateAnimation(GetFrameTime(), 5, 0.7f);
 
             break;
 
-        case 2:
+        case (int)WhichLoredrop::first:
             this->position = {(float)Katara.getPosition().x, (float)Katara.getPosition().y - 20, (float)this->loredropOne.width, (float)this->loredropOne.height};
             UpdateAnimation(GetFrameTime(), 1, 0.7f);
             break;
 
-        case 3:
+        case (int)WhichLoredrop::second:
             this->position = {(float)Katara.getPosition().x, (float)Katara.getPosition().y - 20, (float)this->loredropTwo.width, (float)this->loredropTwo.height};
             UpdateAnimation(GetFrameTime(), 1, 0.7f);
             break;
 
-        case 4:
+        case (int)WhichLoredrop::third:
             this->position = {(float)Katara.getPosition().x, (float)Katara.getPosition().y - 20, (float)this->loredropThree.width, (float)this->loredropThree.height};
             UpdateAnimation(GetFrameTime(), 1, 0.7f);
             break;
@@ -70,21 +70,21 @@ void ActorLoredrop::InternRender()
     // specific for every loredrop
 
 
-    switch (current)
+    switch (currentLoredrop)
     {
-        case 1:
+        case WhichLoredrop::base:
             RenderAnimation({this->position.x, this->position.y, (float)this->loredropBase.width / 5, (float)this->loredropBase.height}, 5, 1);
             break;
 
-        case 2:
+        case WhichLoredrop::first:
             RenderAnimation({this->position.x, this->position.y, (float)this->loredropOne.width, (float)this->loredropOne.height}, 1, 1);
             break;
 
-        case 3:
+        case WhichLoredrop::second:
             RenderAnimation({this->position.x, this->position.y, (float)this->loredropTwo.width, (float)this->loredropTwo.height}, 1, 1);
             break;
 
-        case 4:
+        case WhichLoredrop::third:
             RenderAnimation({this->position.x, this->position.y, (float)this->loredropThree.width, (float)this->loredropThree.height}, 1, 1);
             break;
 
